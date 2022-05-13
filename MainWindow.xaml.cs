4c5312d99b9cd3512cb8e1b84ce5210284407f30
Alexander Pronin <prnnsn@mail.ru>
2022-05-13 13:20:22 +0500

    Some improves for UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -10,20 +11,35 @@ namespace GenImageViewer
 {
     public static class Extensions
     {
-        public static BitmapSource ToWpfBitmap(this Bitmap bitmap)
+        public static BitmapSource ToWpfBitmapSource(this GameResource.TGAFile tgaFile)
         {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                bitmap.Save(stream, ImageFormat.Bmp);
+            var bitmap = tgaFile.GetBitmap();
 
-                stream.Position = 0;
-                BitmapImage result = new BitmapImage();
-                result.BeginInit();
-                result.CacheOption = BitmapCacheOption.OnLoad;
-                result.StreamSource = stream;
-                result.EndInit();
-                result.Freeze();
-                return result;
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    System.Windows.Media.PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
             }
         }
     }
@@ -44,7 +60,6 @@ namespace GenImageViewer
         private System.Windows.Size TGASize;
         private System.Windows.Size ImageSize;
         private Ratio ImageToTGA;
-
 
         public GameResource GameResources;
         public bool IsGrid = false;
@@ -85,14 +100,12 @@ namespace GenImageViewer
             infoTGACount.Text = "1";
             GameResource.TGAFile tgaFile = GameResources.TGAFiles[index];
 
-            Bitmap bitmap = tgaFile.GetBitmap();
-
-            imgTGA.Source = bitmap.ToWpfBitmap();
+            imgTGA.Source = tgaFile.ToWpfBitmapSource();
 
             TGASize = new System.Windows.Size()
             {
-                Width = bitmap.Width,
-                Height = bitmap.Height
+                Width = tgaFile.Width,
+                Height = tgaFile.Height
             };
 
             CalcRatioImageToTGA();
@@ -108,6 +121,7 @@ namespace GenImageViewer
             }
 
             infoMappedImages.Text = tgaFile.MappedImages.Count.ToString();
+            GC.Collect();
         }
 
         private void AddMapedImageControls(GameResource.MappedImage mappedImage)
@@ -127,6 +141,7 @@ namespace GenImageViewer
                     $"  Status = {mappedImage.Status}\r\n" +
                     $"End"
             };
+
             ToolTipService.SetShowDuration(control, 60000);
             ToolTipService.SetPlacement(control, System.Windows.Controls.Primitives.PlacementMode.Top);
 
@@ -147,14 +162,24 @@ namespace GenImageViewer
                     $"  Status = {mappedImage.Status}\r\n" +
                     $"End"
             };
+
             ToolTipService.SetShowDuration(listBoxItem, 60000);
             ToolTipService.SetPlacement(listBoxItem, System.Windows.Controls.Primitives.PlacementMode.Top);
+
+            bool mouseEnter = false;
+            listBoxItem.ToolTipOpening += delegate (object o, ToolTipEventArgs e)
+            {
+                e.Handled = !mouseEnter;
+            };
+
             listBoxItem.MouseEnter += delegate
             {
+                mouseEnter = true;
                 control.Focus();
             };
             listBoxItem.MouseLeave += delegate
             {
+                mouseEnter = false;
                 listBoxItem.Focus();
                 bool f = control.IsFocused;
             };
@@ -223,8 +248,39 @@ namespace GenImageViewer
             }
             control.Width = (mappedImage.Coords.Right - mappedImage.Coords.Left) * tgaToMapped.X * ImageToTGA.X;
             control.Height = (mappedImage.Coords.Bottom - mappedImage.Coords.Top) * tgaToMapped.Y * ImageToTGA.Y;
-            Canvas.SetLeft(control, mappedImage.Coords.Left * tgaToMapped.X * ImageToTGA.X);
-            Canvas.SetTop(control, mappedImage.Coords.Top * tgaToMapped.Y * ImageToTGA.Y);
+
+            double left = mappedImage.Coords.Left * tgaToMapped.X * ImageToTGA.X;
+            if (left < 3)
+            {
+                left = 3;
+                //double w = control.Width - 3;
+                //if (w < 0) w = 0;
+                //control.Width = w;
+            }
+            if (left + control.Width + 3 > ImageSize.Width)
+            {
+                double w = ImageSize.Width - left - 3;
+                if (w < 0) w = 0;
+                control.Width = w;
+            }
+
+            double top = mappedImage.Coords.Top * tgaToMapped.Y * ImageToTGA.Y;
+            if (top < 3)
+            {
+                top = 3;
+                //double h = control.Height - 3;
+                //if (h < 0) h = 0;
+                //control.Height = h;
+            }
+            if (top + control.Height + 3 > ImageSize.Height)
+            {
+                double h = ImageSize.Height - top - 3;
+                if (h < 0) h = 0;
+                control.Height = h;
+            }
+
+            Canvas.SetLeft(control, left);
+            Canvas.SetTop(control, top);
         }
         private void ResizeMappedImageControls()
         {
@@ -260,7 +316,7 @@ namespace GenImageViewer
             using (var dialog = new WK.Libraries.BetterFolderBrowserNS.BetterFolderBrowser
             {
                 Title = "Выберете папку с ресурсами",
-                Multiselect = false   
+                Multiselect = false
             })
             {
                 System.Windows.Forms.DialogResult result = dialog.ShowDialog();
@@ -268,7 +324,7 @@ namespace GenImageViewer
                     return;
 
                 SelectGameFolder(dialog.SelectedPath);
-            }        
+            }
         }
 
         public void SelectGameFolder(string folder)
@@ -300,13 +356,90 @@ namespace GenImageViewer
 
         private ListBoxItem CreateTGAListBoxItem(GameResource.TGAFile tgaFile)
         {
+            // I am forced to use this dirt method because otherwise i get memory leaks in ToolTip image
+            StackPanel GetToolTipContent()
+            {
+                StackPanel stackPanel = new StackPanel()
+                {
+                    Orientation = Orientation.Vertical
+                };
+                Border border1 = new Border()
+                {
+                    BorderThickness = new Thickness(1),
+                    BorderBrush = System.Windows.Media.Brushes.LightGray,
+                    Padding = new Thickness(3),
+                    Margin = new Thickness(3)
+                };
+                TextBlock tb = new TextBlock()
+                {
+                    Text =
+                    $@"{(tgaFile.BIGResource != null ? $@"BIG File = {tgaFile.BIGResource.BIGRFile.FileName}" + "\r\n" : "")}" +
+                            $@"{(string)tgaFile.TGALocation}\{tgaFile.Name}" + "\r\n" + "\r\n" +
+                            $@"Width = {tgaFile.Width}" + "\r\n" +
+                            $@"Height = {tgaFile.Height}" + "\r\n" + "\r\n" +
+                            $@"MappedImages = {tgaFile.MappedImages.Count}"
+                };
+                border1.Child = tb;
+
+                Border border2 = new Border()
+                {
+                    BorderThickness = new Thickness(1),
+                    BorderBrush = System.Windows.Media.Brushes.LightGray,
+                    Padding = new Thickness(3),
+                    Margin = new Thickness(3)
+                };
+                Border border2_1 = new Border()
+                {
+                    BorderThickness = new Thickness(3),
+                    BorderBrush = System.Windows.Media.Brushes.LightGray,
+                    Width = 256,
+                    Height = 256
+                };
+                System.Windows.Controls.Image img = new System.Windows.Controls.Image();
+                border2_1.Child = img;
+                border2.Child = border2_1;
+                img.Source = tgaFile.ToWpfBitmapSource();
+
+                stackPanel.Children.Add(border1);
+                stackPanel.Children.Add(border2);
+
+                return stackPanel;
+            }
+
+            ToolTip toolTip = new ToolTip();
+            {
+                toolTip.Opened += delegate
+                {
+                    toolTip.Content = GetToolTipContent();
+                };
+                toolTip.Closed += delegate
+                {
+                    toolTip.Content = null;
+                    GC.Collect(); // For avoid unnecessary memory leaks (comment up)
+                };
+            }
+
             ListBoxItem listBoxItem = new ListBoxItem()
             {
                 Content = tgaFile.Name,
-                ToolTip =
-                        $@"{(tgaFile.BIGResource != null ? $@"BIG File = {tgaFile.BIGResource.BIGRFile.FileName}" + "\r\n" : "")}" +
-                        $@"{(string)tgaFile.TGALocation}\{tgaFile.Name}"
+                ToolTip = toolTip
             };
+
+            // Fix for open ToolTIp only when mouse enter on item
+            bool mouseEnter = false;
+            listBoxItem.MouseEnter += delegate
+            {
+                mouseEnter = true;
+            };
+            listBoxItem.MouseLeave += delegate
+            {
+                mouseEnter = false;
+            };
+            listBoxItem.ToolTipOpening += delegate (object o, ToolTipEventArgs e)
+            {
+                e.Handled = !mouseEnter;
+            };
+
             ToolTipService.SetShowDuration(listBoxItem, 60000);
             ToolTipService.SetPlacement(listBoxItem, System.Windows.Controls.Primitives.PlacementMode.Top);
 
